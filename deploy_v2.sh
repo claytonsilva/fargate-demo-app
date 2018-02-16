9d4cff07590ade4f306b094fb6860913bf97338b
claytonsilva <clayton@xdevel.com.br>
2018-02-16 07:26:54 -0200

    testing

--- deploy_v2.sh
+++ deploy_v2.sh
@@ -5,4 +5,4 @@ IMAGE_PREFIX="$AWS_ACCOUNT.dkr.ecr.$REGION.amazonaws.com"
 docker tag $TASK:latest $IMAGE_PREFIX/$TASK:$TRAVIS_TAG
 docker push $IMAGE_PREFIX/$TASK:$TRAVIS_TAG
 
-ecs-deploy -c $CLUSTER -r $REGION -c $TASK -n $TASK -i $IMAGE_PREFIX/$TASK:$TRAVIS_TAG
+ecs-deploy -c $CLUSTER -r $REGION -n $TASK -i $IMAGE_PREFIX/$TASK:$TRAVIS_TAG
